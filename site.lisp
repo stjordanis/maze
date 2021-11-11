27ee8d1a46b092a62698a209070eaf12a9b50f96
Susam Pal <susam@susam.in>
2021-11-11 15:42:39 +0530

    Add extra markup to post lists too

--- site.lisp
+++ site.lisp
@@ -287,25 +287,24 @@ value, next-index."
            (end-tag-index)
            (next-index 0))
       (loop
-        (setf begin-tag-index (search "<h" text :start2 next-index))
-        (unless begin-tag-index
-          (return))
-        (cond ((and (digit-char-p (char text (+ begin-tag-index 2)))
-                    (substring-at "id=\"" text (+ begin-tag-index 4)))
+         (setf begin-tag-index (search "<h" text :start2 next-index))
+         (unless begin-tag-index
+           (return))
+         (cond ((and (digit-char-p (char text (+ begin-tag-index 2)))
+                     (substring-at "id=\"" text (+ begin-tag-index 4)))
 
-               (setf end-id-index (search "\"" text
-                                          :start2 (+ begin-tag-index 8)))
-               (setf end-tag-index (search "</h" text
-                                           :start2 (+ end-id-index 2)))
-               (format s "~a" (subseq text next-index end-tag-index))
-               (format s "<a href=\"#~a\"></a></h"
-                       (subseq text (+ begin-tag-index 8) end-id-index))
-               (setf next-index (+ end-tag-index 3)))
-              (t
-               (format s "~a" (subseq text next-index (+ begin-tag-index 2)))
-               (setf next-index (+ begin-tag-index 2)))))
-      (format s (subseq text next-index)))))
-
+                (setf end-id-index (search "\"" text
+                                           :start2 (+ begin-tag-index 8)))
+                (setf end-tag-index (search "</h" text
+                                            :start2 (+ end-id-index 2)))
+                (format s "~a" (subseq text next-index end-tag-index))
+                (format s "<a href=\"#~a\"></a></h"
+                        (subseq text (+ begin-tag-index 8) end-id-index))
+                (setf next-index (+ end-tag-index 3)))
+               (t
+                (format s "~a" (subseq text next-index (+ begin-tag-index 2)))
+                (setf next-index (+ begin-tag-index 2)))))
+      (format s "~a" (subseq text next-index)))))
 
 ;;; Posts
 ;;; -----
@@ -423,7 +422,7 @@ value, next-index."
     (add-canonical-url dst-path params)
     ;; Render the post using list layout.
     (write-log "Rendering list => ~a ..." dst-path)
-    (write-file dst-path (render list-layout params))))
+    (write-file dst-path (extra-markup (render list-layout params)))))
 
 
 ;;; Comments
